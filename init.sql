a46e1f06f2191728cd0ebc93cc14318eb5d738bb
Matt <mqunell@gmail.com>
2020-01-22 11:35:46 -0800

    add Mechagon dungeons

--- init.sql
+++ init.sql
@@ -14,14 +14,14 @@ create table Keystones (
     primary key (RealName, CharName)
 );
 
-insert into Keystones values ("Matt", "Matchi", "Monk", "123", null, null, null, null, null);
-insert into Keystones values ("Matt", "Maios", "DeathKnight", "12", null, null, null, null, null);
-insert into Keystones values ("Jeremy", "Demondude", "Warlock", "2", null, null, null, null, null);
-insert into Keystones values ("Jeremy", "Whitelight", "Paladin", "12", null, null, null, null, null);
-insert into Keystones values ("Kyle", "Aessthetics", "Druid", "13", null, null, null, null, null);
-insert into Keystones values ("Bravo", "ßrâvø", "DemonHunter", "12", null, null, null, null, null);
-insert into Keystones values ("Bravo", "Touka", "Druid", "2", null, null, null, null, null);
-insert into Keystones values ("Ken", "Thagurok", "DeathKnight", "1", null, null, null, null, null);
-insert into Keystones values ("Ken", "Magerina", "Mage", "2", null, null, null, null, null);
-insert into Keystones values ("Sarah", "Risá", "Mage", "2", null, null, null, null, null);
-insert into Keystones values ("Ilan", "Kohi", "Shaman", "23", null, null, null, null, null);
+-- insert into Keystones values ("Matt", "Matchi", "Monk", "123", null, null, null, null, null);
+-- insert into Keystones values ("Matt", "Maios", "DeathKnight", "12", null, null, null, null, null);
+-- insert into Keystones values ("Jeremy", "Demondude", "Warlock", "2", null, null, null, null, null);
+-- insert into Keystones values ("Jeremy", "Whitelight", "Paladin", "12", null, null, null, null, null);
+-- insert into Keystones values ("Kyle", "Aessthetics", "Druid", "13", null, null, null, null, null);
+-- insert into Keystones values ("Bravo", "ßrâvø", "DemonHunter", "12", null, null, null, null, null);
+-- insert into Keystones values ("Bravo", "Touka", "Druid", "2", null, null, null, null, null);
+-- insert into Keystones values ("Ken", "Thagurok", "DeathKnight", "1", null, null, null, null, null);
+-- insert into Keystones values ("Ken", "Magerina", "Mage", "2", null, null, null, null, null);
+-- insert into Keystones values ("Sarah", "Risá", "Mage", "2", null, null, null, null, null);
+-- insert into Keystones values ("Ilan", "Kohi", "Shaman", "23", null, null, null, null, null);
